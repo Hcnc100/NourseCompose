cdca8f1571e77393621256e72019aa623b986da2
Death <ricardo.pajaro.bc2d@gmail.com>
2022-10-01 20:34:19 -0500

    update dependencies

--- app/src/main/java/com/nullpointer/noursecompose/ui/activitys/MainActivity.kt
+++ app/src/main/java/com/nullpointer/noursecompose/ui/activitys/MainActivity.kt
@@ -4,16 +4,12 @@ import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import androidx.lifecycle.lifecycleScope
 import com.nullpointer.noursecompose.ui.screen.NavGraphs
 import com.nullpointer.noursecompose.ui.states.rememberRootAppState
 import com.nullpointer.noursecompose.ui.theme.NourseComposeTheme
 import com.ramcosta.composedestinations.DestinationsNavHost
 import com.ramcosta.composedestinations.navigation.dependency
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.withContext
 
 
 @AndroidEntryPoint
@@ -21,21 +17,10 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        var isSplash = true
-        installSplashScreen().apply {
-            setKeepOnScreenCondition {
-                isSplash
-            }
-        }
-        lifecycleScope.launchWhenCreated {
-            withContext(Dispatchers.IO) {
-                withContext(Dispatchers.IO) { delay(1000) }
-                isSplash = false
-            }
-        }
+        installSplashScreen()
         setContent {
             NourseComposeTheme {
-                val rootAppState= rememberRootAppState()
+                val rootAppState = rememberRootAppState()
                 DestinationsNavHost(
                     navGraph = NavGraphs.main,
                     startRoute = NavGraphs.main.startRoute,
